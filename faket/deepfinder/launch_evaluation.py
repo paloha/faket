925288b75633ccf861934fdc4f7d153e0b6779f6
Pavol Harar <537041-paloha@users.noreply.gitlab.com>
2022-06-25 08:07:05 +0000

    Fixed wrong path building

--- faket/deepfinder/launch_evaluation.py
+++ faket/deepfinder/launch_evaluation.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 from pathlib import Path
 import utils.objl as ol
+from os.path import join as pj
 
 
 #def launch_evaluation(test_tomogram, test_tomo_idx, num_epochs, label_map_path, out_path):
@@ -20,20 +21,26 @@ if __name__ == '__main__':
     parser.add_argument("--out_path", type=str, 
                         help="out path for the xml files resulting from clustering")
     args = parser.parse_args()
+    
+    
+    xml_fname = f'tomo{args.test_tomo_idx}_{args.test_tomogram}_2021_{args.num_epochs}epoch_bin1_objlists_thr.xml'
+    out_fname = f'tomo{args.test_tomo_idx}_{args.test_tomogram}_2021_{args.num_epochs}epoch_bin1.txt'
+    particle_list_fname = f'particle_locations_tomo_{args.test_tomogram}_.txt'
+    objl = ol.read_xml(pj(args.out_path, xml_fname))
 
-    part_file_name = f'{args.out_path}tomo{args.test_tomo_idx}_{args.test_tomogram}_2021_{args.num_epochs}epoch'
-    objl = ol.read_xml(f'{part_file_name}_bin1_objlist_thr.xml')
-    particle_list = f'{args.out_path}particle_locations_tomo_{args.test_tomogram}_.txt'
+    
+    # part_file_name = f'{args.out_path}tomo{args.test_tomo_idx}_{args.test_tomogram}_2021_{args.num_epochs}epoch'
+    # objl = ol.read_xml(f'{part_file_name}_bin1_objlist_thr.xml')
+    # particle_list = f'{args.out_path}particle_locations_tomo_{args.test_tomogram}_.txt'
     
     # convert the predicted object list into a text file, as needed by the SHREC'21 evaluation script:
-       
     class_name = {
         0: "0", 1: "4V94", 2: "4CR2", 3: "1QVR", 4: "1BXN", 5: "3CF3", 6: "1U6G",
         7: "3D2F", 8: "2CG9", 9: "3H84", 10: "3GL1", 11: "3QM1", 12: "1S3X", 
         13: "5MRC",  14: "vesicle", 15: "fiducial"
     }
     
-    with open(particle_list, 'w+') as file:
+    with open(pj(args.out_path, particle_list_fname), 'w+') as file:
         for p in range(0, len(objl)):
             x = int(objl[p]['x'])
             y = int(objl[p]['y'])
@@ -45,9 +52,9 @@ if __name__ == '__main__':
     eval_script = Path('data/shrec2021_extended_dataset/misc/eval.py')
     test_tomogram_folder = Path('data/shrec2021_extended_dataset/model_9/faket/')
     args = [
-        f'-s {particle_list}',
+        f'-s {pj(args.out_path, particle_list_fname)}',
         f'-t {test_tomogram_folder}',
-        f'-o {part_file_name}_bin1_2021.txt'
+        f'-o {pj(args.out_path, xml_fname)}'
     ]
     os.system(f'{interpreter} {eval_script} {" ".join(args)}')
 
